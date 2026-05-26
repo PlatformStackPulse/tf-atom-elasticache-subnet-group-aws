dceb7cb380ba22249b6b5594fd3da4e3ec7f4cd7
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 00:10:54 +0100

    feat: implement tf-atom-elasticache-subnet-group-aws module

--- variables.tf
+++ variables.tf
@@ -1,7 +1,14 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "subnet_ids" {
+  description = "List of subnet IDs for the ElastiCache subnet group"
+  type        = list(string)
+  validation {
+    condition     = length(var.subnet_ids) >= 1
+    error_message = "At least one subnet ID is required."
+  }
+}
+
+variable "description" {
+  description = "Description of the subnet group"
+  type        = string
+  default     = null
+}
